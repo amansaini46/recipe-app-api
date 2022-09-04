02f8267954a2a41f79619b5eb1179f6847d2de84
Aman Saini <amansaini86@gmail.com>
2022-09-03 21:12:08 -0700

    Fixed db check tests

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,6 @@
 import time
 
+from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.db import connections
 from django.core.management.base import BaseCommand
@@ -9,12 +10,13 @@ class Command(BaseCommand):
     """"Wait until DB is available"""
 
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database ....')
-        db_conn = None
-        while not db_conn:
+        self.stdout.write('Waiting for database....')
+        db_up = False
+        while not db_up:
             try:
-                db_conn = connections['default']
-            except OperationalError:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
                 self.stdout.write('Waiting for DB will try again in 1 seconds')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
